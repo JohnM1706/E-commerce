f5642258f0a7b70f7893ab79febe63fbcf1fef82
JhonFredy <jhonfred680@gmail.com>
2023-04-26 20:11:35 -0500

    inicio

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: "AKIA5IRSBWYSBQBC3MY3",
+          secretAccessKey: "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",
+          region:"eu-west-1",
+          params: {
+            Bucket: "ecommerce-stripe",
+          },
+        },
+      },
+    },
+ 
+  });
